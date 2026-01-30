df049557ed492a52d9a7fac576a4f2bc453a790f
MazBaz <mrlog42@gmail.com>
2026-01-30 19:38:47 +0100

    add tailwindcss

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -6,6 +6,10 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div class="p-6 text-red-500 font-bold">
+    Tailwind OK 🚀
+  </div>
 </template>
 
 <style scoped></style>
