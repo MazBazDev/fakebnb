f5d01cb1418af19ebdda6793a6ea94d70e44fd80
MazBaz <mrlog42@gmail.com>
2026-02-06 19:36:28 +0100

    add ci/cd

--- apps/app/src/__tests__/App.spec.ts
+++ apps/app/src/__tests__/App.spec.ts
@@ -8,14 +8,15 @@ import App from '@/App.vue'
 describe('App', () => {
   it('mounts renders properly', async () => {
     const pinia = createPinia()
-    router.push('/')
-    await router.isReady()
 
     const wrapper = mount(App, {
       global: {
         plugins: [pinia, router],
       },
     })
+
+    router.push('/')
+    await router.isReady()
 
     expect(wrapper.text()).toContain('MiniBnB')
   })
