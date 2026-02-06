595f54ba1f5a1c625124353000ff9dd71d90a6b9
MazBaz <mrlog42@gmail.com>
2026-02-07 00:03:52 +0100

    chore: add proxies

--- apps/api/bootstrap/app.php
+++ apps/api/bootstrap/app.php
@@ -13,6 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: ['*'],
+            headers: Request::HEADER_X_FORWARDED_TRAEFIK
+        );
+
         $middleware->redirectGuestsTo(function ($request) {
             if ($request->expectsJson() || $request->is('api/*')) {
                 return null;
